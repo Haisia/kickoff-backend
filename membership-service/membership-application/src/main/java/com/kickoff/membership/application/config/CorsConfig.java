1f06513041233039338e30ebe6479481a8a82825
최준혁 <cwnsgur13579@gmail.com>
2025-02-28 15:08:47 +0900

    수정: 외부IP 오픈

--- membership-service/membership-application/src/main/java/com/kickoff/membership/application/config/CorsConfig.java
+++ membership-service/membership-application/src/main/java/com/kickoff/membership/application/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.kickoff.membership.application.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,10 +10,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+  @Value("${value.allow-ip}")
+  private String KICKOFF_ALLOW_IP;
+
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 출처 추가
+    configuration.addAllowedOrigin("http://"+KICKOFF_ALLOW_IP+":3000");
+
     configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용 (GET, POST 등)
     configuration.addAllowedHeader("*"); // 모든 헤더 허용
     configuration.setAllowCredentials(true); // 인증정보(Cookie 등) 허용
